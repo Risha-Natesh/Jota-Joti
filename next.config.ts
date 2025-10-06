9bd65f6cb9232cfabe22ac83e639d732ac509c6d
RISHA N B Tech 23 <rishan.btech23@rvu.edu.in>
2025-10-06 18:02:11 +0000

    make a public folder and switch/push my logo there

--- next.config.ts
+++ next.config.ts
@@ -28,12 +28,6 @@ const nextConfig: NextConfig = {
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
       }
     ],
   },
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,7 +21,7 @@ const navLinks = [
 
 const Logo = () => (
     <Image 
-        src="https://storage.googleapis.com/stey-public/jota-joti-logo.png" 
+        src="/jota-joti-logo.png" 
         alt="JOTA JOTI Logo"
         width={120}
         height={60}
