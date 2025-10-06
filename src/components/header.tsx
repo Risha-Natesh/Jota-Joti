c8b12c28b6d71f238ee6bbab4812387596283b31
RISHA N B Tech 23 <rishan.btech23@rvu.edu.in>
2025-10-06 15:32:55 +0000

    can u make the color of the navigation bar a lil darker

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,7 +23,7 @@ const ScoutIcon = (props: React.SVGProps<SVGSVGElement>) => (
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background">
+    <header className="sticky top-0 z-50 w-full border-b bg-muted">
       <div className="container flex h-16 items-center">
         <div className="mr-4 flex items-center">
           <Link href="/" className="mr-6 flex items-center space-x-2">
