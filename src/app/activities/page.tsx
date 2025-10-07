070fc124a2ed1fa0c6f85d0e53e7539aa4e9184c
H J Vrishank B Tech 23 <vrishankhj.btech23@rvu.edu.in>
2025-10-07 17:15:34 +0000

    in team games add a bullet point saying that scouts and guides should ha

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -49,9 +49,8 @@ const competitions = [
             time: "11:00 AM - 12:00 PM",
             location: "SCSG",
             locationUrl: "https://maps.app.goo.gl/LbwTZoreEqEuCNMcA",
-            ageGroups: [
-                { age: "12 - 16 YRS", rules: "Requires a team of 10." },
-                { age: "17 - 24 YRS", rules: "Requires a team of 10." },
+            rules: [
+                "Scouts and Guides should be in a patrol of 6 to 8 members."
             ]
         }
     },
