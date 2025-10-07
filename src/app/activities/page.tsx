48ed5cdabdb86d8ec821b96781737242b6024acf
RISHA N B Tech 23 <rishan.btech23@rvu.edu.in>
2025-10-07 16:33:28 +0000

    take out the entire time conponent

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -14,7 +14,6 @@ const competitions = [
             date: "17-Oct-25",
             location: "Panchavati",
             locationUrl: "https://maps.app.goo.gl/cC787aQb9vq7HybE7",
-            duration: "2.5",
             materials: "Drawing Paper",
             ageGroups: [
                 { age: "6 - 9 YRS", rules: "Medium - Crayons, Colour Pencils" },
@@ -31,7 +30,6 @@ const competitions = [
             date: "17-Oct-25",
             location: "Panchavati",
             locationUrl: "https://maps.app.goo.gl/cC787aQb9vq7HybE7",
-            duration: "4 - 6",
             materials: "Table & Chair",
             ageGroups: [
                 { age: "8 - 11 YRS", title: "Cardboard Creations", rules: "Design and build a simple machine, like a catapult or a marble run, using cardboard tubes and boxes." },
@@ -47,7 +45,6 @@ const competitions = [
             date: "17-Oct-25",
             location: "SCSG",
             locationUrl: "https://maps.app.goo.gl/LbwTZoreEqEuCNMcA",
-            duration: "4 - 6",
             materials: "Game Props",
             ageGroups: [
                 { age: "12 - 16 YRS", rules: "Requires a team of 10." },
@@ -138,7 +135,6 @@ export default function ActivitiesPage() {
                                                 ) : (
                                                     <div className="flex items-center gap-2"><MapPin size={14}/> {comp.details.location}</div>
                                                 )}
-                                                <div className="flex items-center gap-2"><Clock size={14}/> {comp.details.duration}</div>
                                              </div>
                                         )}
                                         {comp.details.ageGroups ? (
